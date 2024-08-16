10e81b974dcdb485288e031ecf2811118dac8310
joao-montanari <joao.unipbr@gmail.com>
2024-08-16 07:02:59 -0300

    feat (user api services) Cria as funções que realizam as requisições de CRUD para os usuários

---
+++ src/api/services/users.ts
@@ -0,0 +1,53 @@
+import { User } from '@Models/user';
+import { selectMethod } from '@Api/methods';
+import { findRouter }  from '@Api/mapRouters';
+
+export async function createUser(user : User) {
+    let request = await findRouter("createUser");
+
+    return selectMethod(
+        request.router,
+        request.method,
+        user
+    );
+}
+
+export async function listUsers() {
+    let request = await findRouter("listUsers");
+
+    return selectMethod(
+        request.router,
+        request.method,
+    );
+}
+
+export async function getUser(id : number) {
+    let request = await findRouter("getUser");
+    let url : string = `${request.router}${id}`
+
+    return selectMethod(
+        url,
+        request.method,
+    );
+}
+
+export async function updateUser(user : User) {
+    let request = await findRouter("updateUser");
+    let url : string = `${request.router}${user.idUsuario}`
+
+    return selectMethod(
+        url,
+        request.method,
+        user
+    );
+}
+
+export async function deleteUser(id : number) {
+    let request = await findRouter("deleteUser");
+    let url : string = `${request.router}${id}`
+
+    return selectMethod(
+        url,
+        request.method,
+    );
+}
