e4857f92f614492cf7191cbe093a4086fb975b92
joao-montanari <joao.unipbr@gmail.com>
2024-08-16 06:47:19 -0300

    feat (DeleteModal) Cria o componente DeleteModal baseado no componente Modal

---
+++ src/components/DeleteModal/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ReportGmailerrorred } from "@mui/icons-material";
+
+import Modal from "@Components/Modal";
+
+const DeleteModal = ({ 
+    onDelete, 
+    title,
+    description,
+    open,
+    setOpen,
+} : { 
+    onDelete : any, 
+    title : string,
+    description : string,
+    open : boolean,
+    setOpen : any,
+}) => {
+    return (
+        <Modal
+            isOpen={open}
+            setOpen={setOpen}
+        >
+            <ReportGmailerrorred style={{ color: "#E11D48", width: '55px', height: "55px", backgroundColor: '#e11d4732', padding: '8px', borderRadius: '50%' }} />
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <div style={{ width: '100%' }} >
+            <button  
+                onClick={() => {
+                    onDelete();
+                }} 
+                style={{ backgroundColor: "#E11D48", color: "#FFF" }}
+            >
+                Deletar
+            </button>
+            <button
+                onClick={() => setOpen(false)} 
+                style={{ border: '1px solid #828080', backgroundColor: "#FFF" }}
+            >
+                Cancelar
+            </button>
+            </div>
+        </Modal>
+    );
+}
+
+export default DeleteModal;
