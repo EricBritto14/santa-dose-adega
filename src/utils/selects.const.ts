d330213e0213ea1f8d9a9a9c8f8b6b585f3af0b4
joao-montanari <joao.unipbr@gmail.com>
2024-08-16 07:00:10 -0300

    feat (selects const) Cria nova lista de seleção para os selects do formulário de produtos

--- src/utils/selects.const.ts
+++ src/utils/selects.const.ts
@@ -1,20 +1,42 @@
-
 export type TypeSelect = {
-    value: string,
+    value: string | number,
     label: string,
 }
 
 export const category_select : TypeSelect[] = [
     {
-        value: 'drink',
-        label: 'Bebida',
-    },
-    {
-        value: 'food',
+        value: 'Comida',
         label: 'Comida',
     },
     {
-        value: 'alcoholic',
-        label: 'Alcólica',
+        value: 'Não alcoólicas',
+        label: 'Não alcoólicas',
+    },
+    {
+        value: 'Alcoólicas',
+        label: 'Alcoólicas',
+    },
+    {
+        value: 'Doces',
+        label: 'Doces',
+    },
+]
+
+export const unit_of_measure_select : TypeSelect[] = [
+    {
+        value: 'L',
+        label: 'L',
+    },
+    {
+        value: 'ml',
+        label: 'ml',
+    },
+    {
+        value: 'G',
+        label: 'G',
+    },
+    {
+        value: 'Kg',
+        label: 'Kg',
     },
 ]
